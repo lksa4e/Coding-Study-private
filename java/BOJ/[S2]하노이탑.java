fdc45bb859226542a30d2a7e713a6edf99ceb601
lksa4e <55073084+lksa4e@users.noreply.github.com>
2021-10-29 23:46:35 +0900

    Create [S2]하노이탑.java

---
+++ java/BOJ/[S2]하노이탑.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+public class Main {
+	static int N;
+	static int answer = 0;
+	static StringBuilder sb = new StringBuilder();
+
+	// src에서 temp를 거쳐 target으로 N개
+	static void solve(int src, int temp, int target, int n) {
+		if (n == 1) {
+			sb.append(src + " " + target + "\n");
+			answer++;
+		} else {
+			solve(src, target, temp, n - 1);
+			sb.append(src + " " + target + "\n");
+			answer++;
+			solve(temp, src, target, n - 1);
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		if(N <= 20) {
+			solve(1, 2, 3, N);
+            System.out.println(answer);
+			System.out.print(sb.toString());
+		}else {
+			BigDecimal b = new BigDecimal(2);
+			System.out.println( b.pow(N).subtract(new BigDecimal(1)));
+		}
+	}
+}
